4a1394b1a21235f37c44c744788cb050f9e1dee7
Lee Nelson <lee@parsable.com>
2020-01-08 23:28:53 -0800

    updates themes

--- src/themes.js
+++ src/themes.js
@@ -11,25 +11,25 @@ export const LUNAR_THEME = {
   "custom-primary-a": "#345ecb",
   "custom-primary-b": "#30457f",
   "custom-primary-c": "#323746",
-  "custom-secondary-a": "#323746",
+  "custom-secondary-a": "#345ecb",
   "custom-secondary-b": "#323746",
   "custom-secondary-c": "#fff"
 };
 
 export const EVERGREEN_THEME = {
   "custom-primary-a": "#3fb5a1",
-  "custom-primary-b": "#1b6053",
+  "custom-primary-b": "#04584b",
   "custom-primary-c": "#3fb5a1",
-  "custom-secondary-a": "#293930",
-  "custom-secondary-b": "#1e8573",
+  "custom-secondary-a": "#079865",
+  "custom-secondary-b": "#444a49",
   "custom-secondary-c": "#ffffff"
 };
 
 export const CRIMSON_THEME = {
   "custom-primary-a": "#bb0287",
-  "custom-primary-b": "#850662",
+  "custom-primary-b": "#452577",
   "custom-primary-c": "#bb0287",
-  "custom-secondary-a": "#850662",
+  "custom-secondary-a": "#452577",
   "custom-secondary-b": "#000000",
   "custom-secondary-c": "#fff"
 };
